b48f4ac6e3208a674c8fa86b2e3f02cd85cc8ea3
Carlos Galarza <carloslfu@gmail.com>
2023-07-26 21:31:43 -0500

    discord

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/discord",
+        destination: "https://discord.gg/fdZmGHyxRj",
+        permanent: true,
+      },
+    ]
+  },
 }
