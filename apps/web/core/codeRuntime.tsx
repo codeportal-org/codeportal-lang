2c157bd48403734990dd871ceb9d684f12b0ad41
Carlos Galarza <carloslfu@gmail.com>
2023-09-02 17:27:47 -0500

    remove log

--- apps/web/core/codeRuntime.tsx
+++ apps/web/core/codeRuntime.tsx
@@ -30,8 +30,6 @@ import { html } from "https://esm.sh/htm@3.1.1/react"
 import * as react from "https://esm.sh/react@18.2.0"
 import * as ReactDOM from "https://esm.sh/react-dom@18.2.0/client"
 
-console.log("ReactDOM", ReactDOM)
-
 ${completion}
 `
 }
