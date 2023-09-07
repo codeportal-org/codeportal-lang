78a2f5e2a08ad5d0bdbf57f24b59dc76ff580946
Carlos Galarza <carloslfu@gmail.com>
2023-09-07 09:17:09 -0500

    suppressHydrationWarning

--- apps/web/app/(codeportal)/layout.tsx
+++ apps/web/app/(codeportal)/layout.tsx
@@ -5,13 +5,14 @@ import "@/styles/globals.css"
 
 export const metadata = {
   title: "CodePortal | Turn ideas into reality — faster!",
-  description: "CodePortal is the open-source full-stack programming platform for indie hackers and makers!",
+  description:
+    "CodePortal is the open-source full-stack programming platform for indie hackers and makers!",
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <link rel="shortcut icon" href="/favicon.png" />
         <body>{children}</body>
--- apps/web/app/(sites)/layout.tsx
+++ apps/web/app/(sites)/layout.tsx
@@ -2,7 +2,7 @@ import "./styles.css"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </head>
