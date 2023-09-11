50ca01fd016b721f529b14d337c10fab26200eb0
Carlos Galarza <carloslfu@gmail.com>
2023-09-11 18:04:36 -0500

    remove extra spacing

--- apps/web/app/api/apps/[appId]/completion/route.ts
+++ apps/web/app/api/apps/[appId]/completion/route.ts
@@ -65,196 +65,196 @@ function createSystemPrompt() {
 
 function createUserPrompt(userInput: string) {
   return `Create the web app using the following description which was provided by the user, use these requirements as the source of truth. The user request is:
-  \`\`\`
-  ${userInput}
-  \`\`\`
+\`\`\`
+${userInput}
+\`\`\`
 
-  Give the app a title with a h1 and a meaningful name and description.
+Give the app a title with a h1 and a meaningful name and description.
 
-  Keep titles short and catchy. Use at least a couple of sentences for all other text. Titles and subtitles
-  should be bold and creative. Don’t repeat the user request verbatim in the content. Never use placeholder
-  names like Jane Doe or Acme Inc., instead use real names and companies.
+Keep titles short and catchy. Use at least a couple of sentences for all other text. Titles and subtitles
+should be bold and creative. Don’t repeat the user request verbatim in the content. Never use placeholder
+names like Jane Doe or Acme Inc., instead use real names and companies.
 
-  Never use these characters:
+Never use these characters:
 
-  - \\'
-  - \"
+- \\'
+- \"
 
-  Instead, always use these characters:
+Instead, always use these characters:
 
-  - “
-  - ”
-  - ‘
-  - ’
+- “
+- ”
+- ‘
+- ’
 
-  So never write quotation marks and apostrophes like this:
+So never write quotation marks and apostrophes like this:
 
-  - \"That isn\\'t right\"
-  - can\\'t, isn\\'t, won\\'t
-  - Juan\\' ability
+- \"That isn\\'t right\"
+- can\\'t, isn\\'t, won\\'t
+- Juan\\' ability
 
-  Instead, always write quotation marks and apostrophes like this:
+Instead, always write quotation marks and apostrophes like this:
 
-  - “This’s better!”
-  - can’t, isn’t, won’t
-  - Juan’ ability
+- “This’s better!”
+- can’t, isn’t, won’t
+- Juan’ ability
 
-  Never use the zero width space character (U+200B).
+Never use the zero width space character (U+200B).
 
-  In React when there is a derived value from two states, do not use another state, use a derived value instead.
+In React when there is a derived value from two states, do not use another state, use a derived value instead.
 
-  Do not use React state for static data like the title or the description, inline them in the JS instead.
+Do not use React state for static data like the title or the description, inline them in the JS instead.
 
-  Carefully design what the user requests. Make sure the buttons and the main features work correctly. If there is a customizable list in the requirements, include a way to delete the items. If there are very common easy to implement features that are obvious, implement them. Style the container of the app so it is well aligned and well designed.
+Carefully design what the user requests. Make sure the buttons and the main features work correctly. If there is a customizable list in the requirements, include a way to delete the items. If there are very common easy to implement features that are obvious, implement them. Style the container of the app so it is well aligned and well designed.
 
-  If the user is asking for an app to collect end user data, include all of it including end user submitted data and derived data.
+If the user is asking for an app to collect end user data, include all of it including end user submitted data and derived data.
 
-  Evaluate the user requirements to infer the Backend Data Model, like saving lists or form entries. Give the field names meaningful names, add "id" as a field name, it is a special reserved field. The names of the fields should be names with spaces not camelCase. Take this into account to display the data in the html and components.
+Evaluate the user requirements to infer the Backend Data Model, like saving lists or form entries. Give the field names meaningful names, add "id" as a field name, it is a special reserved field. The names of the fields should be names with spaces not camelCase. Take this into account to display the data in the html and components.
 
-  If saving numbers or other raw data into the state as a list, wrap them in items so if the user wants them to be saved to the cloud it is easier to implement. For example:
-  \`\`\`javascript
-  const [items, setItems] = useState([
-    { id: "1", value: 1 },
-    { id: "2", value: 2 },
-  ])
+If saving numbers or other raw data into the state as a list, wrap them in items so if the user wants them to be saved to the cloud it is easier to implement. For example:
+\`\`\`javascript
+const [items, setItems] = useState([
+  { id: "1", value: 1 },
+  { id: "2", value: 2 },
+])
 
-  // or it could be numbers
-  const [numbers, setNumbers] = useState([
-    { id: "1", value: 1 },
-    { id: "2", value: 2 },
-  ])
+// or it could be numbers
+const [numbers, setNumbers] = useState([
+  { id: "1", value: 1 },
+  { id: "2", value: 2 },
+])
 
-  // save to backend
+// save to backend
 
-  // response from backend, assume the backend returns the new item when creating it with POST
-  const newItem = response1.json()
-  const newNumber = response2.json()
+// response from backend, assume the backend returns the new item when creating it with POST
+const newItem = response1.json()
+const newNumber = response2.json()
 
-  setItems((items) => [...items, newItem])
-  setNumbers((numbers) => [...numbers, newNumber])
+setItems((items) => [...items, newItem])
+setNumbers((numbers) => [...numbers, newNumber])
 
-  // get from the backend
-  const items = await fetch("/api/data/items")
-  const numbers = await fetch("/api/data/numbers")
-  setItems(items)
-  setNumbers(numbers)
-  \`\`\`
+// get from the backend
+const items = await fetch("/api/data/items")
+const numbers = await fetch("/api/data/numbers")
+setItems(items)
+setNumbers(numbers)
+\`\`\`
 
-  If the user does not specify the endpoint where to send form data, submit all the data to this URL '/api/data/{form-name}' as a POST request form body. Take into account the Backend Data Model to know what to send in the form body. Give form-name a descriptive form name for the data. Take this into account if displaying the data in a list or in the UI later.
+If the user does not specify the endpoint where to send form data, submit all the data to this URL '/api/data/{form-name}' as a POST request form body. Take into account the Backend Data Model to know what to send in the form body. Give form-name a descriptive form name for the data. Take this into account if displaying the data in a list or in the UI later.
 
-  When fetching data from an API using the fetch API take into account the response not OK cases such as 404, 400 and 500.
+When fetching data from an API using the fetch API take into account the response not OK cases such as 404, 400 and 500.
 
-  When fetching a list of things from '/api/data' take into account that every item in the list has a unique id plus the body data you sent to the API.
+When fetching a list of things from '/api/data' take into account that every item in the list has a unique id plus the body data you sent to the API.
 
-  Wrap HTTP requests (fetch) inside try-catch to account for errors.
+Wrap HTTP requests (fetch) inside try-catch to account for errors.
 
-  IMPORTANT - If there are possible errors from HTTP requests, display it with a modal or toast. Avoid user technical jargon. Do not user the term "fetch". If it is a toast make it disappear after after the next successful HTTP request.
+IMPORTANT - If there are possible errors from HTTP requests, display it with a modal or toast. Avoid user technical jargon. Do not user the term "fetch". If it is a toast make it disappear after after the next successful HTTP request.
 
-  When using HTTP PATCH send the modified data in the body.
+When using HTTP PATCH send the modified data in the body.
 
-  IMPORTANT - NEVER include the error message in the app. Instead, display a user friendly message. NEVER say, oops, something went wrong, or something similar in an error message. Instead, say what happened, why it happened and what the user can do to fix it.
+IMPORTANT - NEVER include the error message in the app. Instead, display a user friendly message. NEVER say, oops, something went wrong, or something similar in an error message. Instead, say what happened, why it happened and what the user can do to fix it.
 
-  For any button use the Button component, here a TypeScript interface of its props:
-  \`\`\`typescript
-  interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    /** The variant of the button */
-    variant?: 'default' | 'destructive' | 'outline' | 'secondary' | 'ghost' | 'link'
-    /** The size of the button. */
-    size?: 'default' | 'sm' | 'lg' | 'icon'
-    /** When asChild is set to true, the component will not render a default DOM element, instead cloning the part's child and passing it the props and behavior required to make it functional. */
-    asChild?: boolean
-  }
-  \`\`\`
+For any button use the Button component, here a TypeScript interface of its props:
+\`\`\`typescript
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  /** The variant of the button */
+  variant?: 'default' | 'destructive' | 'outline' | 'secondary' | 'ghost' | 'link'
+  /** The size of the button. */
+  size?: 'default' | 'sm' | 'lg' | 'icon'
+  /** When asChild is set to true, the component will not render a default DOM element, instead cloning the part's child and passing it the props and behavior required to make it functional. */
+  asChild?: boolean
+}
+\`\`\`
 
-  If any Button that is inside a flex container add the shrink-0 class to it so it does not shrink.
+If any Button that is inside a flex container add the shrink-0 class to it so it does not shrink.
 
-  For inputs, use the Input component, here is a TypeScript interface of its props:
-  \`\`\`typescript
-  interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
-  \`\`\`
+For inputs, use the Input component, here is a TypeScript interface of its props:
+\`\`\`typescript
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+\`\`\`
 
-  For any checkbox use the Checkbox component. Here is a TypeScript interface of its props:
-  \`\`\`typescript
-  interface CheckboxProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    asChild?:	boolean
-    defaultChecked: boolean
-    checked: boolean
-    onCheckedChange: function
-    disabled: boolean
-    required: boolean
-    name: string
-    value: string
-  }
-  \`\`\`
+For any checkbox use the Checkbox component. Here is a TypeScript interface of its props:
+\`\`\`typescript
+interface CheckboxProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  asChild?:	boolean
+  defaultChecked: boolean
+  checked: boolean
+  onCheckedChange: function
+  disabled: boolean
+  required: boolean
+  name: string
+  value: string
+}
+\`\`\`
 
-  A \`Checkbox\` cannot have children.
+A \`Checkbox\` cannot have children.
 
-  A \`Checkbox\` should always have a label.
+A \`Checkbox\` should always have a label.
 
-  If the UI has a checkbox-like control, make it squared.
+If the UI has a checkbox-like control, make it squared.
 
-  If there is an Input with a possible action when hitting enter, implement the action on enter key functionality. If there is an Input, validate that it cannot be submitted empty.
+If there is an Input with a possible action when hitting enter, implement the action on enter key functionality. If there is an Input, validate that it cannot be submitted empty.
 
-  Do not include the code for the Button or Input components in the app, it is already included in the app.
+Do not include the code for the Button or Input components in the app, it is already included in the app.
 
-  For any toast use the the useToast hook, here is an example:
-  \`\`\`javascript
-  const { toast } = useToast()
+For any toast use the the useToast hook, here is an example:
+\`\`\`javascript
+const { toast } = useToast()
 
-  h('div', null,
-    h('button', {
-      onClick: () => {
-        toast({
-          title: "Scheduled: Catch up",
-          description: "Friday, February 10, 2023 at 5:57 PM",
-        });
-      }
-  }, 'Show Toast')
-  \`\`\`
+return h('div', null,
+  h('button', {
+    onClick: () => {
+      toast({
+        title: "Scheduled: Catch up",
+        description: "Friday, February 10, 2023 at 5:57 PM",
+      });
+    }
+}, 'Show Toast')
+\`\`\`
 
-  Dismiss the toasts automatically after 5 seconds if the user does not dismiss it manually unless explicitly specified otherwise.
+Dismiss the toasts automatically after 5 seconds if the user does not dismiss it manually unless explicitly specified otherwise.
 
-  When creating lists:
-  - The items should be properly aligned, use "justify-between items-center" when necessary.
-  - If the item is using flex any Button directly inside should have the shrink-0 class.
-  - Be sure to always get the list items from the backend using the fetch API and the '/api/data' endpoint.
+When creating lists:
+- The items should be properly aligned, use "justify-between items-center" when necessary.
+- If the item is using flex any Button directly inside should have the shrink-0 class.
+- Be sure to always get the list items from the backend using the fetch API and the '/api/data' endpoint.
 
-  IMPORTANT - You don't include images in the app if the user does not require it explicitly.
+IMPORTANT - You don't include images in the app if the user does not require it explicitly.
 
-  IMPORTANT - You don't include videos in the app if the user does not require it explicitly.
+IMPORTANT - You don't include videos in the app if the user does not require it explicitly.
 
-  IMPORTANT - You don't include audio in the app if the user does not require it explicitly.
+IMPORTANT - You don't include audio in the app if the user does not require it explicitly.
 
-  IMPORTANT - Do not import any library or package. Do not use React.lazy to import any library or package.
+IMPORTANT - Do not import any library or package. Do not use React.lazy to import any library or package.
 
-  Any form or form like interface should be inside a container with the class: max-w-2xl mx-auto. If not, wrap the content of the app in a container with the class: max-w-3xl mx-auto.
+Any form or form like interface should be inside a container with the class: max-w-2xl mx-auto. If not, wrap the content of the app in a container with the class: max-w-3xl mx-auto.
 
-  Be sure to use an async function if the is a use of await inside.
+Be sure to use an async function if the is a use of await inside.
 
-  For mobile the container should have a padding depending on the requirements.
+For mobile the container should have a padding depending on the requirements.
 
-  Use the "outline" variant for delete buttons in lists.
+Use the "outline" variant for delete buttons in lists.
 
-  Do not import Tailwind CSS.
+Do not import Tailwind CSS.
 
-  IMPORTANT - Always use the Button and Input components, do not use HTML input and button elements directly. DO NOT USE "input" or "button". These components are already included in the app, you do not need to import them.
+IMPORTANT - Always use the Button and Input components, do not use HTML input and button elements directly. DO NOT USE "input" or "button". These components are already included in the app, you do not need to import them.
 
-  Always send the JSON header when sending JSON data to the backend:
-  \`\`\`javascript
-  const response = await fetch("/api/data", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ name: "John Doe" }),
-  })
-  \`\`\`
+Always send the JSON header when sending JSON data to the backend:
+\`\`\`javascript
+const response = await fetch("/api/data", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({ name: "John Doe" }),
+})
+\`\`\`
 
-  I’ll start a component for an app which must implement the user specifications and you’ll continue exactly where I left off. Just create the component do not use it:
+I’ll start a component for an app which must implement the user specifications and you’ll continue exactly where I left off. Just create the component do not use it:
 
-  import * from React from "react"
+import * from React from "react"
 
-  const h = React.createElement
+const h = React.createElement
 
-  function App() {`
+function App() {`
 }
