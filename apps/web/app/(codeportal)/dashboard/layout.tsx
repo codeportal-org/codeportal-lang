0c13f00501784c32daeb50d74eddf54904277e09
Carlos Galarza <carloslfu@gmail.com>
2023-09-14 14:39:22 -0500

    add welcome svg

--- apps/web/app/(codeportal)/dashboard/layout.tsx
+++ apps/web/app/(codeportal)/dashboard/layout.tsx
@@ -4,6 +4,7 @@ import { RedirectToSignIn, SignedIn, SignedOut, useUser } from "@clerk/nextjs"
 import React from "react"
 
 import { PageContainer } from "./PageContainer"
+import { WelcomeSVG } from "./WelcomeSVG"
 
 export default function DashboardEntry({ children }: { children: React.ReactNode }) {
   return (
@@ -24,18 +25,19 @@ export function DashboardLayout({ children }: { children: React.ReactNode }) {
 
   return (
     <div className="h-screen">
-      {isInvited || !user ? (
+      {false || !user ? (
         children
       ) : (
         <PageContainer>
           <div className="flex flex-col items-center">
             <div className="flex max-w-2xl flex-col items-center px-2">
-              {
-                <p className="mb-5 max-w-lg text-center text-xl leading-relaxed">
-                  You are in the closed alpha waitlist!
-                  <br /> We will get in touch with you soon!
-                </p>
-              }
+              <p className="mb-5 max-w-lg text-center text-xl leading-relaxed">
+                You are in the closed alpha waitlist!
+                <br /> We will get in touch with you soon!
+              </p>
+              <div className="max-w-md overflow-hidden">
+                <WelcomeSVG />
+              </div>
             </div>
           </div>
         </PageContainer>
