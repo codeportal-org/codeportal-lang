efea85c16c159a2a092b49b2492a745fb0e409af
Carlos Galarza <carloslfu@gmail.com>
2023-09-14 19:56:58 -0500

    fix final codeTree processing

--- apps/web/core/Editor.tsx
+++ apps/web/core/Editor.tsx
@@ -113,7 +113,7 @@ export function Editor({ appId, appName }: { appId: string; appName?: string }) 
         // in case processing was running, reset it
         codeProcessor.reset()
 
-        const ast = codeProcessor.process(code)
+        const codeTree = codeProcessor.process(code)
         console.log("FINAL --- codeTree", codeTree)
         setCodeTree(codeTree)
       }
