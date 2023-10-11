af8ae0349463260aec703b221f6450b2854d7483
Carlos Galarza <carloslfu@gmail.com>
2023-10-10 21:43:52 -0500

    fix escape '

--- apps/web/app/(codeportal)/home/jorge-page/page.tsx
+++ apps/web/app/(codeportal)/home/jorge-page/page.tsx
@@ -4,7 +4,7 @@ const JorgePage = () => {
   return (
     <div className="flex h-screen items-center justify-center bg-blue-500">
       <div className="text-center">
-        <h1 className="mb-5 text-5xl text-white">Welcome to Jorge's Page</h1>
+        <h1 className="mb-5 text-5xl text-white">Welcome to Jorge{`'`}s Page</h1>
         <p className="text-xl text-white">This is a beautiful page created for Jorge.</p>
       </div>
     </div>
