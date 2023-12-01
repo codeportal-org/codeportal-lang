f579860cbdbaa35a9f206a5016b827f2b6829c0a
Carlos Galarza <carloslfu@gmail.com>
2023-12-01 14:43:10 -0500

    add libs

--- packages/lang-core/tsup.config.ts
+++ packages/lang-core/tsup.config.ts
@@ -3,11 +3,10 @@ import { Options, defineConfig } from "tsup"
 export default defineConfig((options: Options) => ({
   treeshake: true,
   splitting: true,
-  entry: ["src/**/*.tsx", "!src/**/*.stories.tsx"],
+  entry: ["src/**/*.ts", "!src/**/*.stories.tsx"],
   format: ["esm", "cjs"],
   dts: true,
   minify: true,
   clean: true,
-  external: ["react"],
   ...options,
 }))
