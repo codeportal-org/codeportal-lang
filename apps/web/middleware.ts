4e401b89b1f9d8625a69b09c2f14b2af257e3222
Carlos Galarza <carloslfu@gmail.com>
2024-01-22 11:47:26 -0500

    remove multitenant setup from middleware

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -26,20 +26,14 @@ export default function middleware(request: NextRequest, event: NextFetchEvent) 
   ) {
     // rewrites for dashboard pages
     if (path.startsWith("/dashboard")) {
-      console.log("->>>> dashboard")
-
       return clerkMiddleware(request, event)
     }
 
     // rewrites for api endpoints (api endpoint to be deprecated soon)
     if (path.startsWith("/api")) {
-      console.log("->>>> api")
-
       return clerkMiddleware(request, event)
-      // return NextResponse.next()
     }
 
-    console.log("->>>> root")
     if (path === "/sign-in-redirect") {
       const url = request.nextUrl.clone()
       url.pathname = "/dashboard"
@@ -56,47 +50,6 @@ export default function middleware(request: NextRequest, event: NextFetchEvent) 
     return NextResponse.rewrite(new URL(`/home${path}`, request.url))
   }
 
-  /**
-   * Sites section
-   */
-
-  const currentHost =
-    process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
-      ? hostname.replace(`.codeportal.io`, "").replace(".codeportal-bice.vercel.app", "")
-      : hostname
-          .replace(`.codeportal.test`, "")
-          .replace(`.0.0.0.0:8090`, "")
-          .replace(".localhost:8090", "")
-
-  // Just dev URLs start with dev-
-  if (currentHost.startsWith("dev-")) {
-    console.log("->>>> dev sites", `/dev-sites/${currentHost}${path}`, request.url)
-
-    if (path.startsWith("/api")) {
-      const oldUrl = new URL(request.url)
-      const newUrl = new URL(`/dev-sites-api/${currentHost}${path}`, oldUrl.origin)
-      newUrl.search = oldUrl.search
-      return NextResponse.rewrite(newUrl)
-    }
-
-    const oldUrl = new URL(request.url)
-    const newUrl = new URL(`/dev-sites/${currentHost}${path}`, oldUrl.origin)
-    newUrl.search = oldUrl.search
-    return NextResponse.rewrite(newUrl)
-  }
-
-  // Production sites
-
-  if (path.startsWith("/api")) {
-    const oldUrl = new URL(request.url)
-    const newUrl = new URL(`/sites-api/${currentHost}${path}`, oldUrl.origin)
-    newUrl.search = oldUrl.search
-    return NextResponse.rewrite(newUrl)
-  }
-
-  console.log("->>>> sites", `/sites/${currentHost}${path}`, request.url)
-  const oldUrl = new URL(request.url)
-  const newUrl = new URL(`/sites/${currentHost}${path}`, oldUrl.origin)
-  newUrl.search = oldUrl.search
-  return NextResponse.rewrite(newUrl)
+  // not found
+  return NextResponse.next()
 }
